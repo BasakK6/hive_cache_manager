71c8ad16d3cd3b3e61de35975d2e8aebc79f8131
BasakK6 <katukbasak@gmail.com>
2023-01-18 20:15:34 +0300

    added test cases using Book objects and Book Cache Manager & deleted widget_test.dart

---
+++ example/test/hive_autoincrement_keys_test.dart
@@ -0,0 +1,67 @@
+import 'package:example/features/books/model/book.dart';
+import 'package:example/project/hive_constants.dart';
+import 'package:example/features/books/cache/book_cache_manager.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hive/hive.dart';
+
+void main() {
+  test('Hive Cache Manager Test', () async {
+    // initialize Hive before using
+    Hive.init(HiveConstants.hiveDbName);
+
+    // create your model objects
+    Book firstBook = Book(
+      title: 'Lord of The Rings - The Fellowship of the Ring',
+      author: 'J. R. R. Tolkien',
+      year: 1954,
+      isbn: '9780618260515',
+    );
+
+    Book secondBook = Book(
+      title: 'Lord of The Rings - The Two Towers',
+      author: 'J. R. R. Tolkien',
+      year: 1954,
+      isbn: '9780261102361',
+    );
+
+    Book thirdBook = Book(
+      title: 'Lord of The Rings - The Return of the King',
+      author: 'J. R. R. Tolkien',
+      year: 1955,
+      isbn: '9780007136575',
+    );
+
+    // create and explicitly initialize a manager object
+    BookCacheManager bookCacheManager = BookCacheManager("${HiveConstants.booksHiveBoxName}_autoincrement_keys");
+    await bookCacheManager.init();
+
+    // clear all the objects written from the previous test run
+    await bookCacheManager.clearAll();
+
+    // the hive box should be empty since no items have been added yet
+    expect(bookCacheManager.getValues(),[]);
+
+    // add your model objects to hive
+    List<Book> books = [firstBook, secondBook, thirdBook];
+    await bookCacheManager.addItems(items: books);
+
+    // test the functionality of the hive_cache_manager
+    expect(bookCacheManager.getValues().first.isbn,'9780618260515');
+
+    // remove the object named "thirdBook" from the hive
+    // since the objects were added with a autoincrement key the key of the "thirdBook" should be 2 (first key is 0)
+    bookCacheManager.removeItem(key: 2);
+
+    // there should be only 2 books left in the hive box
+    expect(bookCacheManager.getValues().length, 2);
+
+    // read the values whose keys start at 0 and ends at 1 (inclusive)
+    Iterable<Book> otherBooks = bookCacheManager.getValuesBetween(startKey: 0, endKey: 1);
+
+    // put the remaining model objects in an Iterable
+    Iterable<Book> remainingBooks = [firstBook, secondBook];
+
+    // "otherBooks" and "remainingBooks" should be equal
+    expect(otherBooks, remainingBooks);
+  });
+}
---
+++ example/test/hive_model_specific_keys_test.dart
@@ -0,0 +1,73 @@
+import 'package:example/features/books/model/book.dart';
+import 'package:example/project/hive_constants.dart';
+import 'package:example/features/books/cache/book_cache_manager.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hive/hive.dart';
+
+void main() {
+  test('Hive Model Specific Keys Test', () async {
+    // initialize Hive before using
+    Hive.init(HiveConstants.hiveDbName);
+
+    // create your model objects
+    Book firstBook = Book(
+      title: 'Lord of The Rings - The Fellowship of the Ring',
+      author: 'J. R. R. Tolkien',
+      year: 1954,
+      isbn: '9780618260515',
+    );
+
+    Book secondBook = Book(
+      title: 'Lord of The Rings - The Two Towers',
+      author: 'J. R. R. Tolkien',
+      year: 1954,
+      isbn: '9780261102361',
+    );
+
+    Book thirdBook = Book(
+      title: 'Lord of The Rings - The Return of the King',
+      author: 'J. R. R. Tolkien',
+      year: 1955,
+      isbn: '9780007136575',
+    );
+
+    // create and explicitly initialize a manager object
+    BookCacheManager bookCacheManager = BookCacheManager("${HiveConstants.booksHiveBoxName}_model_specific_keys");
+    await bookCacheManager.init();
+
+    //clear all the objects written from the previous test run
+    await bookCacheManager.clearAll();
+
+    // the hive box should be empty since no items have been added yet
+    expect(bookCacheManager.getValues(),[]);
+
+    // add your model objects to hive box
+    /*
+    await bookCacheManager.putItem(item: firstBook);
+    await bookCacheManager.putItem(item: secondBook);
+    await bookCacheManager.putItem(item: thirdBook);
+    */
+    bookCacheManager.putItems(items: [firstBook, secondBook, thirdBook]);
+
+    // test the functionality of the hive_cache_manager
+    expect(bookCacheManager.getItem(key: thirdBook.mapKey)?.year, 1955);
+
+    // remove a specific object from the hive box
+    bookCacheManager.removeItem(key: secondBook.mapKey);
+
+    // there should be only 2 books left in the hive box
+    expect(bookCacheManager.getValues().length, 2);
+
+    // remove the rest of the books
+    await bookCacheManager.removeItems(keys: [firstBook.mapKey, thirdBook.mapKey]);
+
+    // the hive box should be empty
+    expect(bookCacheManager.getValues().length, 0);
+
+    // close the hive box
+    await bookCacheManager.closeBox();
+
+    // the hive box should not be open
+    expect(bookCacheManager.isHiveBoxOpen, false);
+  });
+}
