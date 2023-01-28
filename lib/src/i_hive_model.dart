19332ed995a3ba519acadea561867b9f30b53eb5
BasakK6 <katukbasak@gmail.com>
2023-01-28 19:04:48 +0300

    style: formatted files

--- lib/src/i_hive_model.dart
+++ lib/src/i_hive_model.dart
@@ -3,7 +3,7 @@ import 'package:hive/hive.dart';
 /// A type which is used by ICacheManager class.
 ///
 /// Custom model classes should be extended by this class in order to be used by the ICacheManager.
-abstract class IHiveModel extends HiveObject{
+abstract class IHiveModel extends HiveObject {
   /// A custom key specific to the [IHiveModel] object.
   ///
   /// This key is used when putting a [IHiveModel] object into the hive box.
